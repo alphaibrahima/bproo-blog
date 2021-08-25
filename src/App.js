9d67b16a4601bfbb6e993bce9412fcc0398b5f7b
alphaibrahima <alphaibrahimas95@gmail.com>
2021-08-25 17:08:53 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 
 function App() {
   // logique
+  const titre     = "Bonjour voici mon blog!!";
+  const paragraph = "C'est un blog qui parle de technologie...";
+  const lien = "https://www.facebook.com/Invictus.Consulting";
 
   // Template 
   return (
     <div className="App">
       <div className="contenu">
-        <h2>Bonjour voici mon blog!!</h2>
-        <p>c'est un blog qui parle de technologie</p>
+        <h2> { titre } </h2>
+        <p> { paragraph } </p>
+        <p>{lien} </p>
+        <p>{Math.random() * 10}</p>
+        <a href={lien}>Aller liker ma page facebook.</a>
       </div>
     </div>
   );
