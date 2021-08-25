3c6aa4d719e1e711220a712d77cc3dcd6cbd6cac
alphaibrahima <alphaibrahimas95@gmail.com>
2021-08-25 15:59:45 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // logique
+
+  // Template 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenu">
+        <h2>Bonjour voici mon blog!!</h2>
+        <p>c'est un blog qui parle de technologie</p>
+      </div>
     </div>
   );
 }
